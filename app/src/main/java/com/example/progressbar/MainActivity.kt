9540ee873f80c239b72d58ae4fcc93e7259ffb29
Caradhina <zolbooagain@gmail.com>
2019-05-21 09:11:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.progressbar
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), fragmentOne.OnFragmentInteractionListener {
+    override fun onStartProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    override fun onStopProgressBar() {
+        progressBar.visibility = View.GONE
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragmentOne())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
